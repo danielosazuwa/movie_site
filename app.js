75acb3c7ee7bd0cce90783efcc78004181d286f3
unknown <danielosazuwa1@gmail.com>
2023-10-23 22:14:42 +0100

    dynamic page number implemented on the popular movies

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import axios from "axios";
+import _ from "lodash";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const APIKey = "api_key=ed7c4374cf9628a19e0132636e35c40b";
 app.get("/", async (req, res) => {
   try {
+    const randomNum = Math.floor(Math.random() * (200 - 0 + 1) + 0);
+
     const response = await axios.get(
-      "https://api.themoviedb.org/3/movie/popular?api_key=ed7c4374cf9628a19e0132636e35c40b"
+      "https://api.themoviedb.org/3/movie/popular?sort_by=popularity.desc&" +
+        APIKey +
+        "&page=" +
+        randomNum
     );
 
+    const result = response.data;
     const all = await axios.get(
-      "https://api.themoviedb.org/3/trending/all/week?api_key=ed7c4374cf9628a19e0132636e35c40b"
+      "https://api.themoviedb.org/3/trending/all/week?" + APIKey
     );
 
     // const random = allResult[Math.floor(Math.random() * allResult.length)];
@@ -29,10 +36,10 @@ app.get("/", async (req, res) => {
     }
     const allResult = all.data.results;
     const random = getMultipleRandom(allResult, 5);
-    console.log(getMultipleRandom(allResult, 5));
+    // console.log(getMultipleRandom(allResult, 5));
     // const randomTop = Math.random(Math.floor(topRatedResult.length));
     // const topRatedResult = topRatedRes.data;
-    const result = response.data;
+
     // console.log(result);
     // console.log(random);
     res.render("index.ejs", {
@@ -44,6 +51,29 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", {
       error: error.message,
     });
+  }
+});
+
+app.get("/movie/:movieName", async (req, res) => {
+  try {
+    const requestedmovieName = _.lowerCase(req.params.movieName);
+    console.log(requestedmovieName);
+    const movieDetails = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=ed7c4374cf9628a19e0132636e35c40b`
+    );
+    const details = movieDetails.data.results;
+    details.forEach((result) => {
+      const storedTitle = _.lowerCase(result.title);
+      console.log(storedTitle);
+      if (storedTitle === requestedmovieName) {
+        res.render("movieSingle.ejs", {
+          display: result,
+        });
+        console.log("Match found!");
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
   }
 });
 
